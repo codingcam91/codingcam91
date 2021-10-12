10258c71b74c109db6b41b3cd0498fbdcca490b5
Coding_Cam <Cameroncclark91@gmail.com>
2021-10-12 18:13:42 -0400

    Add files via upload
    naive_forecast
    seasonal_naive_forecast

---
+++ naive_forecast_snaive_forecast.R
@@ -0,0 +1,15 @@
+read.csv('features.csv')
+read.csv('stores.csv')
+read.csv('train_2.csv')
+foo_ts = ts(train$Weekly_Sales, frequency = 52, start =c(2010,6))
+
+
+# Naive Forecast
+naive_forecast <- naive(foo_ts,12)
+plot(naive_forecast)
+
+# Lets try Seasonal Naive
+snaive_forecast <- snaive(foo_ts,12)
+plot(snaive_forecast)
+
+
